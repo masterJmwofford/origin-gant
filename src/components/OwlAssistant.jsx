b528dd60ebf691334d1ca1932ba473bc0c874c2e
Jordan Wofford <wofford.codes@gmail.com>
2026-07-07 22:57:08 -0500

    week3updates:

--- src/components/OwlAssistant.jsx
+++ src/components/OwlAssistant.jsx
@@ -72,7 +72,7 @@ export default function OwlAssistant({ activeTab, navItems, onNavigate }) {
         <p>{assistantReply}</p>
       </div>
 
-      <form className="owl-form" onSubmit={submitMessage}>
+      {/* <form className="owl-form" onSubmit={submitMessage}>
         <input
           type="text"
           value={message}
@@ -81,7 +81,7 @@ export default function OwlAssistant({ activeTab, navItems, onNavigate }) {
           aria-label="Ask the owl guide where to navigate"
         />
         <button type="submit">Go</button>
-      </form>
+      </form> */}
 
       <div className="owl-shortcuts">
         {navItems.map((item) => (
